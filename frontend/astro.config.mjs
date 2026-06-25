2b4255389b15c5cd376508e5eacba942db2761de
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2026-06-25 21:14:03 +0600

    feat: update Tailwind CSS integration to use postcss and adjust related configurations

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,13 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwindcss from "@tailwindcss/vite";
-
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
   server: {
     host: "0.0.0.0",
     port: 4321,
